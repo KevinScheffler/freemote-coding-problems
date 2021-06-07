1336183592be140ad3a13ffe1c11f8505ddab6d2
KevinScheffler <schefk2122@gmail.com>
2021-06-07 09:42:11 -0400

    Finished up to countOdd

--- script.js
+++ script.js
@@ -37,9 +37,60 @@ var sumOfPositive = function(nums) {
 // console.log(sumOfPositive([4, 40, -40, -4]));
 
 var multiplyNumbers = function(num1, num2) {
-  return num1 * num2;
+  let product = 0;
+  for (i = 0; i < num1; i++) {
+    product += num2;
+  }
+  return product;
 }
 
-console.log(multiplyNumbers(1, 0));
-console.log(multiplyNumbers(5, 5));
-console.log(multiplyNumbers(100, 500));
+// console.log(multiplyNumbers(1, 0));
+// console.log(multiplyNumbers(5, 5));
+// console.log(multiplyNumbers(100, 500));
+
+var repeatString = function(s, n) {
+  let newString = '';
+  for (let i = 0; i < n; i++) {
+    newString += s;
+  }
+  return newString;
+}
+
+// console.log(repeatString("Abc", 3));
+// console.log(repeatString("B", 5));
+// console.log(repeatString("hello", 2));
+
+// Is Palindrome
+var isPalindrome = function(str) {
+  for (let i = 0; i < str.length / 2; i++) {
+    const rightIndex = str.length - 1 - i;
+    if (str[i] !== str[rightIndex]) {
+      return false
+    }
+  }
+  return true;
+}
+
+// console.log(isPalindrome('racecar'))
+// console.log(isPalindrome('abcd'))
+// console.log(isPalindrome('Abcba'))
+
+// CountOdd
+// var countOdd = function(n) {
+//   let odd = 0;
+//   while (n--) {
+//     if (n % 2 === 1) {
+//       odd++;
+//     }
+//   }
+//   return odd;
+// }
+
+var countOdd = function(n) {
+  return Math.floor(n / 2);
+}
+
+
+console.log(countOdd(5));
+console.log(countOdd(10));
+console.log(countOdd(1));
